0a0ed52264ace96294d3d69a0a4cbb817cfd0ed4
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-12-07 03:29:23 +0530

    Commit 2

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -57,7 +57,8 @@ public class WhatsappRepository {
     public int createMessage(String content) {
         ++messageId;
 
-        Message message = new Message(messageId, content, new Date());
+        Message message = new Message(messageId, content);
+        message.setTimestamp(new Date());
 
         return message.getId();
     }
